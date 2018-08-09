bed0b2ed42d90cef861dd3dadc12ae8752161c7d
Ridita Nizam <riditan@gmail.com>
2018-08-09 21:02:17 +0000

    initial commit

---
+++ site.js
@@ -0,0 +1,35 @@
+// Any time an element with the class of "tagline" is clicked
+// $("#jQtest").click(function(){
+//     $(".jQblock").animate({
+//         height: 'toggle'
+//     }, 1500);
+// });
+
+$(".menuitemname").click(
+        function() {
+            $(".ingredients").animate(
+                {
+                height: 'toggle'
+                }, 
+                1500
+            );
+        }
+    )
+;
+
+$("#showallingredients").click(
+        function() {
+            $(".ingredients").animate(
+                {
+                height: 'toggle'
+                }, 
+                1500
+            );
+        }
+    )
+;
+
+
+
+
+    
