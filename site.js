3f66981858281ba9de3092fe07911eb7cfb92111
Ridita Nizam <riditan@gmail.com>
2018-08-17 19:51:31 +0000

    Added information

--- site.js
+++ site.js
@@ -5,31 +5,31 @@
 //     }, 1500);
 // });
 
+// JQUERY UI LIBRARY SCRIPT TO HAVE GRADIENT SLIDE EFFECT ON INDEX.HTML
+$(window).on("load", function() {
+    $("#gradientslide").hide("slide", 1000);
+});
+
 $(".menuitemname").click(
-        function() {
-            $(".ingredients").animate(
-                {
+    function() {
+        $(".ingredients").animate({
                 height: 'toggle'
-                }, 
-                1500
-            );
-        }
-    )
-;
+            },
+            1500
+        );
+    }
+);
 
 $("#showallingredients").click(
-        function() {
-            $(".ingredients").animate(
-                {
+    function() {
+        $(".ingredients").animate({
                 height: 'toggle'
-                }, 
-                1500
-            );
-        }
-    )
-;
+            },
+            1500
+        );
+    }
+);
 
-
-
-
-    
+$(document).ready(function() {
+	$(window).fadeThis();
+});
