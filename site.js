8e30cc2462716c9eb3441809efe9d247bc73fcc4
Ridita Nizam <RiditaN@gmail.com>
2020-06-03 21:34:26 -0700

    Update site.js

--- site.js
+++ site.js
@@ -1,10 +1,3 @@
-// Any time an element with the class of "tagline" is clicked
-// $("#jQtest").click(function(){
-//     $(".jQblock").animate({
-//         height: 'toggle'
-//     }, 1500);
-// });
-
 // JQUERY UI LIBRARY SCRIPT TO HAVE GRADIENT SLIDE EFFECT ON INDEX.HTML
 $(window).on("load", function() {
     $("#gradientslide").hide("slide", 1000);
